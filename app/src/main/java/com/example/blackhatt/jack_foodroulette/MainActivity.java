ff4c0a9eb2c72941075a0eb46d5f459d30eff4d5
jackw8128 <jackw8128@gmail.com>
2015-07-22 22:32:25 -0700

    Jack splashscreen and EULA

--- app/src/main/java/com/example/blackhatt/jack_foodroulette/MainActivity.java
+++ app/src/main/java/com/example/blackhatt/jack_foodroulette/MainActivity.java
@@ -9,13 +9,13 @@ import android.view.MenuItem;
 
 
 
+
+
+
+@Override
+
+
 public class MainActivity extends ActionBarActivity {
-
-
-
-
-
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
